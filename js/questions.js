b71226e6163d70f394930b8f1048537ee47ada18
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-04 14:59:00 +0100

    Array of questions created

---
+++ js/questions.js
@@ -0,0 +1,61 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What Does HTML Stands For?",
+    answer: "C. Hyper Text Markup Language",
+    option: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Multi Language",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "What Does HTML Stands For?",
+    answer: "C. Hyper Text Markup Language",
+    option: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Multi Language",
+    ],
+  },
+
+  {
+    numb: 3,
+    question: "What Does HTML Stands For?",
+    answer: "C. Hyper Text Markup Language",
+    option: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Multi Language",
+    ],
+  },
+
+  {
+    numb: 4,
+    question: "What Does HTML Stands For?",
+    answer: "C. Hyper Text Markup Language",
+    option: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Multi Language",
+    ],
+  },
+
+  {
+    numb: 5,
+    question: "What Does HTML Stands For?",
+    answer: "C. Hyper Text Markup Language",
+    option: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Text Multi Language",
+    ],
+  },
+];
