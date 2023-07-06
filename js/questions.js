e5e1a81a9a285ce8c92dbfd56099eaa80b9496b4
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-06 14:16:38 +0100

    Developer Quiz WebPage - Completed

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,7 @@
 let questions = [
   {
     numb: 1,
-    question: "What Does HTML Stands For?",
+    question: "What Does HTML Stand For?",
     answer: "C. Hyper Text Markup Language",
     option: [
       "A. Hyper Type Multi Language",
@@ -10,52 +10,34 @@ let questions = [
       "D. Hyper Text Multi Language",
     ],
   },
-
   {
     numb: 2,
-    question: "What Does HTML Stands For?",
-    answer: "C. Hyper Text Markup Language",
-    option: [
-      "A. Hyper Type Multi Language",
-      "B. Hyper Text Multiple Language",
-      "C. Hyper Text Markup Language",
-      "D. Hyper Text Multi Language",
-    ],
+    question: "Which programming language is used for styling web pages?",
+    answer: "B. CSS",
+    option: ["A. HTML", "B. CSS", "C. JavaScript", "D. Python"],
   },
-
   {
     numb: 3,
-    question: "What Does HTML Stands For?",
-    answer: "C. Hyper Text Markup Language",
+    question: "What is the purpose of JavaScript?",
+    answer: "B. To add interactivity and dynamic functionality to web pages",
     option: [
-      "A. Hyper Type Multi Language",
-      "B. Hyper Text Multiple Language",
-      "C. Hyper Text Markup Language",
-      "D. Hyper Text Multi Language",
+      "A. To create web page layouts",
+      "B. To add interactivity and dynamic functionality to web pages",
+      "C. To define the structure of web pages",
+      "D. To store and manage data on the server",
     ],
   },
-
   {
     numb: 4,
-    question: "What Does HTML Stands For?",
-    answer: "C. Hyper Text Markup Language",
-    option: [
-      "A. Hyper Type Multi Language",
-      "B. Hyper Text Multiple Language",
-      "C. Hyper Text Markup Language",
-      "D. Hyper Text Multi Language",
-    ],
+    question: "Which tag is used to create an unordered list in HTML?",
+    answer: "A. ul",
+    option: ["A. ul", "B. ol", "C. li", "D. dl"],
   },
-
   {
     numb: 5,
-    question: "What Does HTML Stands For?",
-    answer: "C. Hyper Text Markup Language",
-    option: [
-      "A. Hyper Type Multi Language",
-      "B. Hyper Text Multiple Language",
-      "C. Hyper Text Markup Language",
-      "D. Hyper Text Multi Language",
-    ],
+    question:
+      "Which programming language is commonly used for server-side web development?",
+    answer: "D. PHP",
+    option: ["A. HTML", "B. CSS", "C. JavaScript", "D. PHP"],
   },
 ];
